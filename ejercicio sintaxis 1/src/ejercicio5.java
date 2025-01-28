c184de93555e2c7baf874434871d3174d3cd9af2
dulce1306 <123046615@upq.edu.mx>
2025-01-28 15:16:32 -0600

    ejercicio3 terminado

---
+++ ejercicio sintaxis 1/src/ejercicio5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ejercicio5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce el número de payasos vendidos: ");
+        int numPayasos = sc.nextInt();
+        System.out.print("Introduce el número de muñecas vendidas: ");
+        int numMuñecas = sc.nextInt();
+        int pesoTotal = (numPayasos * 112) + (numMuñecas * 75);
+        System.out.println("El peso total del paquete es: " + pesoTotal + " gramos.");
+        sc.close();
+    }
+}
+
