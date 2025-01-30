4da1e0efd949c8a364cb10d9616e9147a6f7b385
dulce1306 <123046615@upq.edu.mx>
2025-01-30 14:58:54 -0600

    primer tarea terminada

---
+++ PROYECTO2/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String passwordGuardada="1234567890";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa tu contraseña: ");
+        String passwordUsuario = scanner.nextLine();
+
+        if (passwordGuardada.equalsIgnoreCase(passwordUsuario))
+        {
+            System.out.println("La contraseña coincide");
+        }else{
+
+            System.out.println("la contraseña no coincide");
+        }
+        scanner.close();
+    }
+}
