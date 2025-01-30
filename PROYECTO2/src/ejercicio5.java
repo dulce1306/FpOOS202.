6b160fb59770390ad1965ea65f4fcf05886b4ca3
dulce1306 <123046615@upq.edu.mx>
2025-01-30 15:36:41 -0600

    ejercicio5 completado

--- PROYECTO2/src/ejercicio5.java
+++ PROYECTO2/src/ejercicio5.java
@@ -1,2 +1,46 @@
+import java.util.Scanner;
+
 public class ejercicio5 {
-}
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce el primer número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Introduce el segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Introduce un operador (+, -, *, /): ");
+        char operador = scanner.next().charAt(0);
+
+        double resultado;
+
+        switch (operador) {
+            case '+':
+                resultado = num1 + num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case '-':
+                resultado = num1 - num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case '*':
+                resultado = num1 * num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                    System.out.println("Resultado: " + resultado);
+                } else {
+                    System.out.println("Error: División por cero no permitida.");
+                }
+                break;
+            default:
+                System.out.println("Operador no válido.");
+                break;
+        }
+
+        scanner.close();
+    }
+    }
