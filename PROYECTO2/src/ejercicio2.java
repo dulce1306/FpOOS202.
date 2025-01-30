dbbd209e2e32623f10be1400d660ac7961973005
dulce1306 <123046615@upq.edu.mx>
2025-01-30 15:11:42 -0600

    ejercicio2 terminado

---
+++ PROYECTO2/src/ejercicio2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa un numero entero: ");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0){
+            System.out.println("El numero  es par.");
+        }else {
+            System.out.println("El numero es impar ");
+        }
+        scanner.close();
+    }
+}
