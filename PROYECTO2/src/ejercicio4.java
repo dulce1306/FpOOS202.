c7d4298c4d7a8fabcbf8d5c702370ff1dc4ee8fd
dulce1306 <123046615@upq.edu.mx>
2025-01-30 15:32:22 -0600

    ejercicio4 terminado

--- PROYECTO2/src/ejercicio4.java
+++ PROYECTO2/src/ejercicio4.java
@@ -1,2 +1,46 @@
+import java.util.Scanner;
+
 public class ejercicio4 {
-}
+
+
+    public static boolean validPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Ingrese una cadena: ");
+                String cadena = scanner.nextLine();
+                scanner.close();
+
+                if (esPalindromo(cadena)) {
+                    System.out.println("La cadena es un palíndromo.");
+                } else {
+                    System.out.println("La cadena no es un palíndromo.");
+                }
+            }
+
+            public static boolean esPalindromo(String cadena) {
+                cadena = cadena.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+                int izquierda = 0, derecha = cadena.length() - 1;
+
+                while (izquierda < derecha) {
+                    if (cadena.charAt(izquierda) != cadena.charAt(derecha)) {
+                        return false;
+                    }
+                    izquierda++;
+                    derecha--;
+                }
+                return true;
+            }
+        }
+
+
+
+
